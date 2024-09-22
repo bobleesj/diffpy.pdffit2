7b4b19988078c6326b1dd3f30cc083665ca3c185
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-09-22 14:10:19 -0400

    Successful test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,11 +1,7 @@
 
 import diffpy.pdffit2
-from importlib.metadata import version
 
 def test_import():
-    from importlib.metadata import version
-    __version__ = version("diffpy.pdffit2")
-
     from diffpy.pdffit2 import pdffit2_ext
     from diffpy.pdffit2.output import redirect_stdout
     from diffpy.pdffit2.pdffit import PdfFit
